630afdf798d879eec27bd52ab6e577d4e921fe0a
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-06 19:20:05 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,35 @@
 def division(num1, num2)
- 
+12/2
+
 end
 
 def assign_variable(value)
-  
+value
+
 end
 
-def argue
-  
+def argue(phrase)
+phrase
+
 end
 
-def greeting
+def greeting(greeting,name)
+  puts "#{greeting} ejcbecb #{name}"
 
 end
 
 def return_a_value
+  (return_a_value="Nice")
+
 
 end
 
 def last_evaluated_value
-
+last_evaluated_value="expert"
 end
 
-def pizza_party
+def pizza_party(choice="cheese")
+  puts choice
+  pizza_party=choice
 
 end
